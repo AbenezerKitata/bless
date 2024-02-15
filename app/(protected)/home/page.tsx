95afe709973fd8f4196f50c4015aaaf1964472f8
AbenezerKitata <Abi4kit@gmail.com.com>
2024-02-15 01:29:05 -0800

    added more shadcn ui elements

--- app/(protected)/home/page.tsx
+++ app/(protected)/home/page.tsx
@@ -2,14 +2,20 @@ import { auth } from "@clerk/nextjs";
 import { UserButton } from "@clerk/nextjs";
 import React from "react";
 import ProtectedHeader from "./components/header";
+import { DatePickerDemo } from "@/components/date-picker";
+// import { NavigationMenuDemo } from "@/components/nav-menu";
 
 const ProtectedHomePage = () => {
   const session = auth();
   return (
-    <div>
+    <div className="flex justify-center">
       {session.userId && (
-        <div>
+        <div className="flex flex-col items-center gap-4 justify-center">
           <ProtectedHeader />
+          {/* <NavigationMenuDemo /> */}
+          <div className="w-full flex ml-10 mt-7">
+            <DatePickerDemo />
+          </div>
         </div>
       )}
     </div>
